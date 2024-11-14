4da5f55674cd6d9645d8cf22f8bf6add3ef73bb2
Jakob Brodersen Gugel <mikkelbro95@gmail.com>
2024-11-14 21:21:31 +0100

    Videreudvikling af portfolie-side
    Arrangeret iconer og fonte i mappen assets.
    
    Ændret styling, tilføjet contact-formular

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function setInitialMode() {
   const savedMode = localStorage.getItem('theme');
   if (savedMode === 'light') {
       document.body.classList.add('light-mode');
-      toggleButton.src = 'lightmode.svg';
+      toggleButton.src = 'assets/lightmode.svg';
       icon.style.filter = 'invert(70%) brightness(0%) contrast(100%)';
       
     } else {
       document.body.classList.remove('light-mode');
-      toggleButton.src = 'darkmode.svg';
+      toggleButton.src = 'assets/darkmode.svg';
       icon.style.filter = 'invert(0%) brightness(70%) contrast(100%)';
 
   }
@@ -23,11 +23,11 @@ function toggleMode() {
 
   if (isLightMode) {
       localStorage.setItem('theme', 'light');
-      toggleButton.src = 'lightmode.svg';
+      toggleButton.src = 'assets/lightmode.svg';
       icon.style.filter = 'invert(70%) brightness(0%) contrast(100%)';
     } else {
       localStorage.setItem('theme', 'dark');
-      toggleButton.src = 'darkmode.svg'; 
+      toggleButton.src = 'assets/darkmode.svg'; 
       icon.style.filter = 'invert(0%) brightness(70%) contrast(100%)';
     }
 }
